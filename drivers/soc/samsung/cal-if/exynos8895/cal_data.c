364c3a577b9c2493dbc86037757bfbd5e09fbdd4
nhglong <hoanglong.lumi404@gmail.com>
2025-02-14 17:00:02 +0000

    drivers: cal-if: Bring 8895 up-to-date with others

--- drivers/soc/samsung/cal-if/exynos8895/cal_data.c
+++ drivers/soc/samsung/cal-if/exynos8895/cal_data.c
@@ -24,5 +24,47 @@
 #include "acpm_dvfs_exynos8895.h"
 #include "asv_exynos8895.h"
 
+#if defined(CONFIG_SEC_DEBUG)
+#include <soc/samsung/exynos-pm.h>
+#endif
+
+#if defined(CONFIG_SEC_DEBUG)
+int asv_ids_information(enum ids_info id)
+{
+	int res;
+
+	switch (id) {
+	case tg:
+		res = asv_get_table_ver();
+		break;
+	case lg:
+		res = asv_get_grp(dvfs_cpucl1);
+		break;
+	case bg:
+		res = asv_get_grp(dvfs_cpucl0);
+		break;
+	case g3dg:
+		res = asv_get_grp(dvfs_g3d);
+		break;
+	case mifg:
+		res = asv_get_grp(dvfs_mif);
+		break;
+	case lids:
+		res = asv_get_ids_info(dvfs_cpucl1);
+		break;
+	case bids:
+		res = asv_get_ids_info(dvfs_cpucl0);
+		break;
+	case gids:
+		res = asv_get_ids_info(dvfs_g3d);
+		break;
+	default:
+		res = 0;
+		break;
+	};
+	return res;
+}
+#endif
+
 void (*cal_data_init)(void) = NULL;
 int (*cal_check_hiu_dvfs_id)(u32 id) = NULL;
